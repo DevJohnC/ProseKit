fd8ef7eae76713488bbdc16d30f102729325232b
John Carruthers <johnc@frag-labs.com>
2020-03-26 11:07:23 -0500

    Add AspNetCore project to solution and move src around to make sense.

---
+++ src/ProseKit.AspNetCore/TagHelpers/ProseEditorTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace ProseKit.AspNetCore.TagHelpers
+{
+	[HtmlTargetElement("prose-editor", Attributes = SchemaAttributeName, TagStructure = TagStructure.WithoutEndTag)]
+	public class ProseEditorTagHelper : TagHelper
+	{
+		private const string SchemaAttributeName = "schema";
+
+		public Schema Schema { get; set; } = Schema.Empty;
+
+		public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			return base.ProcessAsync(context, output);
+		}
+	}
+}
